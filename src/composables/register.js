87bcaaac14ce6e6d8e68602bcf89083a5fb5604e
Airam <airam.dev@gmail.com>
2021-02-14 16:55:44 +0000

    Merge Fix Branch

--- src/composables/register.js
+++ src/composables/register.js
@@ -1,34 +1,9 @@
 import { ref } from 'vue'
-<<<<<<< HEAD
-import { auth } from '../firebase/config'
-import { projectFirestore } from '../firebase/config'
-=======
 import { auth, projectFirestore } from '../firebase/config'
->>>>>>> a0c33714a0986d180e35b7d288fb05e40fdb67d4
 
 const error = ref(null)
 const firestore = projectFirestore
 
-<<<<<<< HEAD
-const register = async (email, password, name, address) => {
-    error.value = null
-
-    try {
-        const res = await auth.createUserWithEmailAndPassword(email, password)
-        .then(registeredUser => {
-            firestore.collection("usersCollection")
-            .add({
-              uid: registeredUser.user.uid,
-              name: name,
-              address: address
-            })
-        }).catch(function(error){
-            console.log(error)
-        })
-        error.value = null
-
-        return res
-=======
 const register = async (email, password, name, phone, address) => {
     error.value = null
 
@@ -48,7 +23,6 @@ const register = async (email, password, name, phone, address) => {
                 address: address
             })
         })
->>>>>>> a0c33714a0986d180e35b7d288fb05e40fdb67d4
     } catch(err) {
         console.log(err.message)
         error.value = err.message
